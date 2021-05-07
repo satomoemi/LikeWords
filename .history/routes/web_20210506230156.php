53d185fb6616ad14c46ded9c40b68d882c7c155c
satomoemi <meiyin6888@gmail.com>
2021-05-07 19:11:43 +0900

    initial commit

---
+++ .history/routes/web_20210506230156.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+/*
+|--------------------------------------------------------------------------
+| 1) User 認証不要
+|--------------------------------------------------------------------------
+*/
+Route::get('/', function () {
+    return redirect('/home');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 2) User ログイン後
+|--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => 'auth:user'], function() {
+Route::get('/home', 'HomeController@index')->name('home');});
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
