d9470056e5fea25984684cb1656ace77a15d6e92
satomoemi <meiyin6888@gmail.com>
2021-05-09 23:33:09 +0900

    ユーザー、管理者一覧のview

---
+++ .history/app/Http/Controllers/Admin/HomeController_20210509225304.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function user_index()
+    {
+        return view('admin.home');
+    }
+
+    public function unsubsc_reason_index()
+    {
+        return view('admin.unsubsc/reason');
+    }
+}
