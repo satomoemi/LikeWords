82667b0625f61d7c8a171659812f8a459cf63043
satomoemi <meiyin6888@gmail.com>
2021-05-12 23:50:03 +0900

    退会処理系完了

---
+++ .history/app/Http/Controllers/HomeController_20210512174540.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\WithdrawalRequest;
+use App\Unsubscribe;
+use UserEdit_Operation_DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('user.home');
+    }
+
+    public function unsubsc()
+    {
+        return view('user.unsubsc');
+    }
+
+    public function Withdrawal(WithdrawalRequest $request){
+        //退会処理を追加するメソッド
+        $id = auth::id();
+        $UserEdit_Operation_DB = new UserEdit_Operation_DB();
+        return $UserEdit_Operation_DB->Withdrawal($request,$id);
+    }
+
+
+}
---
+++ .history/app/Http/Controllers/HomeController_20210512181858.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\WithdrawalRequest;
+use App\Unsubscribe;
+use UserEdit_Operation_DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('user.home');
+    }
+
+    public function unsubsc()
+    {
+        
+        $auth = auth::user();
+        return view('user.unsubsc',['auth'=>$auth]);
+    }
+
+    public function Withdrawal(WithdrawalRequest $request){
+        //退会処理を追加するメソッド
+        $id = auth::id();
+        $UserEdit_Operation_DB = new UserEdit_Operation_DB();
+        return $UserEdit_Operation_DB->Withdrawal($request,$id);
+    }
+
+
+}
