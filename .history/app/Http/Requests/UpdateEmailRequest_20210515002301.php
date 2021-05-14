da3e99b31a76c7d0bb9f16a8205cf999a2bb8bc2
satomoemi <meiyin6888@gmail.com>
2021-05-15 01:01:05 +0900

    push.home view ok

---
+++ .history/app/Http/Requests/UpdateEmailRequest_20210515002301.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEmailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'Name' => 'required|email|max:100',
+            'Email' => 'required|email|max:100',
+            'Birthday' => 'required|email|max:100',
+            'Email' => 'required|email|max:100',
+        ];
+    }
+}
---
+++ .history/resources/views/user/push.blade_20210514222100.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="py-5 text-center">
+    <div class="container">
+      <div class="row">
+        <div class="mx-auto col-md-8">
+          <h2 contenteditable="true" class="">通知設定</h2>
+          <p class="lead"> Wordを毎日・ランダムに通知します </p> 
+          通知時間<input type="time" name="push-time"><br> アプリ全体に対して通知　<input type="radio" class="btn-check" name="pushON" id="push1">
+          <label class="pushON" for="push1">ON</label>
+          <input type="radio" class="btn-check" name="pushOFF" id="push2">
+          <label class="pushOFF" for="push2">OFF</label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">更新</button>
+  </div>
+</div>
+@endsection
