db2d458b5e19b917e326b95dd73b6d6050711989
satomoemi <meiyin6888@gmail.com>
2021-05-22 14:15:08 +0900

    フォルダワード一覧、フォルダ作成実装中

---
+++ .history/app/Http/Controllers/WordController_20210522034508.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Word;
+
+class WordController extends Controller
+{
+    //Word作成画面
+    public function CreateWordForm()
+    {
+        return view('user.CreateWord');
+    }
+
+    //Word作成post
+    public function CreateWord(int $id, Request $request)
+    {
+        $this->validate($request, Word::$rules);
+        $word = new Word();
+        $form = $request->all();
+
+        unset($form['_token']);
+        unset($form['$folders_id']);
+        
+        $word->fill($form);
+        $word->save();
+
+        return redirect('/home');
+    }
+}
---
+++ .history/app/Http/Controllers/WordController_20210522040953.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Word;
+
+class WordController extends Controller
+{
+    //Word作成画面
+    public function CreateWordForm(int $id)
+    {
+        return view('user.CreateWord',[
+            'folders_id' => $id
+        ]);
+    }
+
+    //Word作成post
+    public function CreateWord(int $id, Request $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $this->validate($request, Word::$rules);
+        $word = new Word();
+        $form = $request->all();
+
+        unset($form['_token']);
+        
+        $word->fill($form);
+        $current_folder->()->save($task);
+
+        return redirect('/home',[
+            'id' => $current_folder->id,
+        ]);
+    }
+}
