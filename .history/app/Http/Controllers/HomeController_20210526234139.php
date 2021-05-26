bea2b176df0c3a9bc276a64b1989646ca49fea38
satomoemi <meiyin6888@gmail.com>
2021-05-27 00:34:34 +0900

    削除処理完了

---
+++ .history/app/Http/Controllers/HomeController_20210526234139.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Folder;
+use App\User;
+use App\Push;
+use App\Word;
+
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    //フォルダ,Word一覧画面
+    public function home(Request $request)
+    {
+        $folders = Folder::all();
+        // dd($folder);
+        $current_folder = NULL;
+        if($request->id != ''){
+            //$request->idと一致するfolderテーブルのレコード全てを取得
+            $current_folder = Folder::find($request->id);
+
+            //Folderテーブルのwords()を取得 $current_folderで一致するFolderのレコードを出してる、そのFolderのidと一致するwordのレコードを＄wordsに代入
+            $words = $current_folder->words; 
+        }else{
+            $words = NULL;
+        }
+
+        return view('user.home',['folders' => $folders,'current_folder'=>$current_folder,'words'=>$words]);
+        
+    }
+
+
+
+    //フォルダ作成画面
+    public function CreateFolderForm()
+    {
+        return view('user.CreateFolder');
+    }
+
+    //フォルダ作成post
+    public function CreateFolder( Request $request)
+    {
+        
+        $this->validate($request, Folder::$rules);
+        $folders = new Folder();
+        $form = $request->all();
+
+        unset($form['_token']);
+        
+        $folders->fill($form);
+        $folders->save();
+        
+
+        return redirect('/home');
+    }
+
+    //フォルダ編集画面
+    
+    {
+
+    }
+
+    //フォルダ削除
+    public function FolderDelete(Request $request)
+  {
+      // 該当するFolder Modelを取得
+      $folder = Folder::find($request->id);
+
+      // 削除する
+      $folder->delete();
+      
+      return redirect('/home');
+  }  
+
+   
+
+
+}
---
+++ .history/database/migrations/2021_05_21_205504_create_words_table_20210526220139.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('word');
+            $table->text('memo');
+            $table->unsignedBigInteger('folder_id');
+            // $table->integer('push_id');
+            $table->timestamps();
+
+            // 外部キー制約を設定する
+            //他のテーブルとの結びつきを表現するためのカラムに設定,folder_idにはfoldersのidの値しか入れることができない(入れることはできるけどエラーになる)(ここではFolderテーブルが親でword)
+            $table->foreign('folder_id')->references('id')->on('folders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
