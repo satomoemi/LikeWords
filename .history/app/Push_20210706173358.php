52fc5e6c211e3cbb2b59aee2adc15ff186f2fd58
satomoemi <meiyin6888@gmail.com>
2021-07-07 01:09:15 +0900

    push_time表示実装中

---
+++ .history/app/Push_20210706173358.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Push extends Model
+{
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'user_id' => 'required',
+        'player_id' => 'required',
+        'push_time' => 'required',
+        
+        
+    );
+
+    public function user()
+{
+    return $this->belongsTo('App\User');//一つのpushに対して一人のuser hasmany
+}
+
+}
---
+++ .history/database/migrations/2021_06_20_115421_add_player_i_d_to_users_table_20210703133446.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPlayerIDToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('player_id')->nullable(true);
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //書かないとエラーになる
+            $table->string('player_id')->nullable(true);
+        });
+    }
+}
