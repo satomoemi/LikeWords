41f0a1532dc4a8da56a87dce40dc44309579c594
satomoemi <meiyin6888@gmail.com>
2021-07-09 04:22:17 +0900

    pushtime画面表示で問題あり

---
+++ .history/database/migrations/2021_05_17_182956_create_folders_table_20210707212949.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');//wordsとfoldersが外部制約キーかけてるからfoldersを
+        Schema::dropIfExists('folders');
+    }
+}
---
+++ .history/database/migrations/2021_05_21_205504_create_words_table_20210707212615.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('folder_id');
+            $table->string('word');
+            $table->text('memo')->nullable(true);
+            $table->timestamps();
+
+            // 外部キー制約を設定する
+            //他のテーブルとの結びつきを表現するためのカラムに設定,folder_idにはfoldersのidの値しか入れることができない(入れることはできるけどエラーになる)(ここではFolderテーブルが親でWordテーブルが子)
+            //onDelete('cascade')：外部キー制約に CASCADE を割り当てれば、紐づいている先のレコードが削除された際に一緒に削除される
+            $table->foreign('folder_id')->references('id')->on('folders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+        
+    }
+}
