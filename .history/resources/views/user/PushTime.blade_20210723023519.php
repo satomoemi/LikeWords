3b607216763ecaa5b6f739f4e49dcf9c0652ac74
satomoemi <meiyin6888@gmail.com>
2021-07-23 03:17:18 +0900

    デザイン修正

---
+++ .history/resources/views/user/PushTime.blade_20210723023519.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+
+@if($push->doesntExist())
+  <div class="container">
+    <div class="row justify-content-center text-white">
+      <h2>通知の登録をしてから通知時間を設定してください</h2>
+    </div> 
+    <div class="row justify-content-center text-white"> 
+      <p>右下のベルマークで登録できます</p>
+    </div>
+  </div>
+@else
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header bg-white"><h3>通知時間設定</h3>
+              <p class="lead"> Wordを毎日・ランダムに通知します </p> <!--leadは文字を大きくする-->
+          </div>
+
+          <div class="card-body bg-">
+            <form method="POST" action="/push/time">
+            @csrf
+
+              <div class="form-group row">
+                <label for="push_time" class="col-md-4 col-form-label text-md-right">通知時間</label>
+
+                <div class="col-md-6">
+                  <input type="time" name="push_time" {{ $pushtime != NULL ? "value={$pushtime}" : "" }}  class="form-control @error('push_time') is-invalid @enderror">
+
+                  @error('push_time')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+                </div>
+              </div>
+
+              <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                  <button type="submit" class="btn btn-outline-dark">更新</button>
+                </div>
+              </div>
+
+              
+          
+            </form>
+          </div>
+              @if (session('status'))
+                  <div class="alert alert-success">
+                      {{ session('status') }}
+                  </div>
+              @endif
+        </div>
+      </div>
+    </div>
+  </div>
+  
+@endif
+@endsection
---
+++ .history/resources/views/user/UserEdit.blade_20210723022956.php
@@ -0,0 +1,142 @@
+@extends('layouts.app')
+@section('content')
+<body>
+
+
+
+
+
+@if (session('status'))
+    {{ session('status') }}
+@endif
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header bg-white"><h3>ユーザー情報・設定</h3></div>
+                    <div class="card-body bg-dark">
+
+                        <form method="POST" action="{{ route('edit.name') }}">
+                        @csrf  
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right text-white">ユーザー名変更</label>
+                                <div class="col-md-6">
+                                    <input id="name" name="name" value="{{$auth["name"]}}" class="form-control @error('name') is-invalid @enderror">
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <input type="hidden" name="UserId" value={{$auth["id"]}}><!--画面には表示しなくていいけど送りたい値があるとき-->
+                                <button dusk="view-button" class="btn btn-outline-light">更新</button>
+                            </div>
+
+                        </form>
+                        <form method="POST" action="{{ route('edit.email') }}">
+                        @csrf  
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right text-white">メール変更</label>
+                                <div class="col-md-6">
+                                    <input id="email" name="email" value="{{$auth["email"]}}" class="form-control @error('email') is-invalid @enderror">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <input type="hidden" name="UserId" value={{$auth["id"]}}>
+                                <button dusk="view-button" class="btn btn-outline-light">更新</button>
+                            </div>
+
+                        </form>
+                        <form method="POST" action="{{ route('edit.birthday') }}">
+                        @csrf  
+                            <div class="form-group row">
+                                <label for="birthday" class="col-md-4 col-form-label text-md-right text-white">生年月日変更</label>
+                                <div class="col-md-6">
+                                    <input id="birthday" type="date" name="birthday" value="{{$auth["birthday"]}}" class="form-control @error('birthday') is-invalid @enderror">
+                                    @error('birthday')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <input type="hidden" name="UserId" value={{$auth["id"]}}>
+                                <button dusk="view-button" class="btn btn-outline-light">更新</button>
+                            </div>
+
+                        </form>
+                        <form method="POST" action="{{ route('edit.gender') }}">
+                        @csrf  
+                            <div class="form-group row">
+                                <label for="gender" class="col-md-4 col-form-label text-md-right text-white">性別変更</label>
+                                <div class="col-md-6">
+                                    <input  id="gender" type="radio" name="gender" class="btn-check text-white @error('gender') is-invalid @enderror"  value="男" {{ $auth["gender"] =='男'? "checked" : "" }} > 男
+
+                                    <input id="gender1" type="radio" name="gender" class="btn-check text-white @error('gender') is-invalid @enderror" value="女" {{ $auth["gender"] =='女'? "checked" : "" }} > 女
+
+                                    <input id="gender2" type="radio" name="gender" class="btn-check text-white @error('gender') is-invalid @enderror"  value="答えたくない" {{ $auth["gender"] == '答えたくない'? "checked" : "" }}>
+                                    答えたくない
+
+
+                                    @error('gender')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <input type="hidden" name="UserId" value={{$auth["id"]}}>
+                                <button dusk="view-button" class="btn btn-outline-light">更新</button>
+                            </div>
+
+                        </form>
+
+                        <form method="POST" action="{{ route('edit.password') }}">
+                        @csrf
+                            <div class="form-group row">
+                                <label for="password"  class="col-md-4 col-form-label text-md-right text-white">現在のパスワードを入力</label>
+                                <div class="col-md-6">
+                                    <input  type="password" id="password"  name="CurrentPassword" class="form-control @error('CurrentPassword') is-invalid @enderror">
+                                    @error('CurrentPassword')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right text-white">新規パスワードを入力</label>
+                                <div class="col-md-6">
+                                    <input　 type="password" id="password" name="newPassword" class="form-control @error('password') is-invalid @enderror" name="newPassword">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right text-white">新規パスワードを再入力</label>
+                                <div class="col-md-6">
+                                    <input  type="password" id="password" name="newPassword_confirmation" class="form-control @error('newPassword') is-invalid @enderror" name="newPassword">
+                                    @error('newPassword')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <input type="hidden" name="UserId" value={{$auth["id"]}}>
+                                <button dusk="view-button" class="btn btn-outline-lig">更新</button>
+
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+@endsection
