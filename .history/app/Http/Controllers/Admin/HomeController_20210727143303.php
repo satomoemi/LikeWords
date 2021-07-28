bf3c597e0d7bab62c4ba972d87c9f1a35de00c53
satomoemi <meiyin6888@gmail.com>
2021-07-28 13:29:07 +0900

    adminの登録画面、処理作成中

---
+++ .history/app/Http/Controllers/Admin/HomeController_20210727143303.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Admin;
+use App\User;
+use App\Unsubscribe;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    //admin登録画面
+    public function RegisterView()
+    {
+        return view('admin.register');
+    }
+
+    //admin登録
+    public function register(Request $request)
+    {
+        return redirect(route('admin.login'));
+    }
+    //ユーザー一覧
+    public function user_index(Request $request)
+    {
+        $users = User::all();
+
+        return view('admin.home', ['user_indexes' => $users]);
+    }
+
+    public function unsubsc_reason_index(Request $request)
+    {
+        $unsubsc_reasons = Unsubscribe::all();
+
+        return view('admin.unsubsc_reason', ['unsubsc_reason_indexes' => $unsubsc_reasons]);
+    }
+}
