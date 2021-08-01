182eb69235ea12b0c1db4993ac3baf39ad33c8b3
satomoemi <meiyin6888@gmail.com>
2021-08-02 03:00:30 +0900

    ファビコン設置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <title>{{ config('app.name') }}</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('likewords.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('LikeWords.ico') }}">
 
 
     <!-- Scripts -->
