c4f689f75528dbc79f03575b6c066ed4c711234a
satomoemi <meiyin6888@gmail.com>
2021-08-01 17:54:29 +0900

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <title>{{ config('app.name') }}</title>
-    <link rel="icon" href="{{asset('favicon.ico')}}">
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}">
 
 
     <!-- Scripts -->
