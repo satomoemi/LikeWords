3faf366b9ec66464288f76f37015e95b47334787
satomoemi <meiyin6888@gmail.com>
2021-08-14 01:45:46 +0900

    home.blade tableレスポンシブ

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -37,8 +37,8 @@
 
 <div class="container py-5">
     <div class="row">
-        <div class="col-12 col-md-6">
-            <table class="table table-hover">
+        <div class=" col-md-6">
+            <table class="table table-hover table-responsive">
                 <thead class="bg-white">
                     <tr class="text-dark mb-9">
                         <th>
@@ -74,7 +74,7 @@
         </div>
 
         <div class="col-12 col-md-6">
-            <table class="table table-white table-hover">
+            <table class="table table-white table-hover table-responsive">
                 @if($current_folder != NULL)
                 <thead class="bg-white">
                     <tr>
